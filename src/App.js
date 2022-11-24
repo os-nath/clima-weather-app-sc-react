818b8dce2fc71e980520ef18d9540eee3526d236
Nathasha <nsoares@corpcloud.com.au>
2022-11-24 15:33:44 +0800

    added gitHub repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Olaaa!</h1>
-      </header>
+      <h1>Clima Tempo</h1>
+      <footer>
+        This project was coded by Nath Soares and is
+        <a
+          href="https://github.com/os-nath/clima-weather-app-sc-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
