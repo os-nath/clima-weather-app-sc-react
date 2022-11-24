f178ee52974d8ffd23b814291de57f5a2e9f368a
Nathasha <nsoares@corpcloud.com.au>
2022-11-24 15:45:43 +0800

    added more footer links

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ function App() {
     <div className="App">
       <h1>Clima Tempo</h1>
       <footer>
-        This project was coded by Nath Soares and is
+        Open source code on{" "}
         <a
           href="https://github.com/os-nath/clima-weather-app-sc-react"
           target="_blank"
           rel="noreferrer"
         >
-          open sourced on GitHub
+          GitHub{" "}
+        </a>{" "}
+        by
+        <a
+          href="https://www.linkedin.com/in/nathasha-soares-1327275a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Nath Soares
+        </a>{" "}
+        hosted on
+        <a
+          href="https://637f1f9f48c65532b6f796d4--wondrous-pothos-41872e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        > Netlify
         </a>
       </footer>
     </div>
